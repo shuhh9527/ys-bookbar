e09f11780d9129837e510e448433541b6054187f
yangzz999 <@yzyan912@163.com>
2021-02-22 09:48:06 +0800

    修改vue.config.ts

--- vue.config.ts
+++ vue.config.ts
@@ -1,23 +1,23 @@
-const path = require('path');
 module.exports = {
-  configureWebpack: {
-    resolve: { extensions: [".ts", ".tsx", ".js", ".json"] },
-    module: {
-      rules: [
-        {
-          test: /\.tsx?$/,
-          loader: 'ts-loader',
-          exclude: /node_modules/,
-          options: {
-            appendTsSuffixTo: [/\.vue$/],
-          }
-        }
-      ]
-    }
-  },
+  // configureWebpack: {
+  //   resolve: { extensions: [".ts", ".tsx", ".js", ".json"] },
+  //   module: {
+  //     rules: [
+  //       {
+  //         test: /\.tsx?$/,
+  //         loader: 'ts-loader',
+  //         exclude: /node_modules/,
+  //         options: {
+  //           appendTsSuffixTo: [/\.vue$/],
+  //         }
+  //       }
+  //     ]
+  //   }
+  // },
   chainWebpack: (config) => {
-    config.entryPoints.clear() // 清空默认入口
-    config.entry('test').add(path.getPath('./src/main.ts')) // 重新设置
+    config.entry('app')
+      .clear()
+      .add('./src/main.ts')
   }
 
 }
